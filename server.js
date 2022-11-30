15d025e08794d6b3e954d5b989b645defb80aa85
Frances OBrien <francey@Francess-iMac.local>
2022-11-30 10:59:11 -0500

    tip calculator express application created

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/greeting/:firstname', (req, res) => {
     console.log(req.params);
     res.send('Hello! Welcome! How are you, ' +req.params.firstname + '?')
 })
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let tipAmt = (req.params.total * req.params.tipPercentage) / 100;
+    res.send('You should tip $' + tipAmt)
+})
 
 //tell app where to listen, confirm listening
 app.listen(3000, () => {
