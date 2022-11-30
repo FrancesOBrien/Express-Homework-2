ed038d85fb0ccad11871dd80df834396d7ed441d
Frances OBrien <francey@Francess-iMac.local>
2022-11-30 11:52:00 -0500

    magic 8 ball express application created

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const replies = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 
 //configure the app (app.set)
 
@@ -14,7 +15,10 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     let tipAmt = (req.params.total * req.params.tipPercentage) / 100;
     res.send('You should tip $' + tipAmt)
 })
-
+app.get('/magic/:query', (req, res) =>{ 
+    let response = replies[Math.floor(Math.random() * replies.length)]
+    res.send(`<h1>${req.params.query}  <br>  ${response}</h1>`)
+})
 //tell app where to listen, confirm listening
 app.listen(3000, () => {
     console.log('listening now');
